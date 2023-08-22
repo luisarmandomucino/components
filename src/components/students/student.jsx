6ef13263e218faba4dc89b5805ce68a44529c6c9
luis muciño <nievesmucino@gmail.com>
2023-08-22 17:05:39 -0600

    destructurando los props

--- src/components/students/student.jsx
+++ src/components/students/student.jsx
@@ -2,16 +2,20 @@ import React from "react";
 
 const studentName = "Tania";
 
-// Los datos pasados como props, los recibimos como un objeto
+// Los datos pasados como props, los recibimos como un objeto en un parametro de la función 
 
-const Student = (props) => {
-  console.log(props);
+
+// la desestructuración es obtener las claves de un objeto para hacer asignaciones menos complejas.
+
+//{name, url} = props; 
+
+const Student = ({name, url, isGraduate}) => {
   return (
     <>
-      <a href={props.url} target="_blank">
-        <h4>{props.name}</h4>
+      <a href={url} target="_blank">
+        <h4>{name}</h4>
       </a>
-      <p> Ex-alumno: {props.isGraduate.toString()}</p>
+      <p> Ex-alumno: {isGraduate.toString()}</p>
     
     </>
   );
